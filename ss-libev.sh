1ef61230b49a8cc06e835ddc0358e942df8fcb9c
debiansid <9624961+debiansid@users.noreply.github.com>
2026-09-14 11:52:20 +0800

    Update ss-libev.sh

--- ss-libev.sh
+++ ss-libev.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-export VER_SSLIBEV="3.3.5"
+export VER_SSLIBEV="3.3.6"
 export VER_SIPOBFS="0.0.5"
-export VER_MBEDTLS="2.28.0"
-export VER_SODIUM="1.0.18"
+export VER_MBEDTLS="4.2.0"
+export VER_SODIUM="1.0.22"
 export VER_PCRE="8.45"
 export VER_EV="4.33"
-export VER_CARES="1.19.0"
+export VER_CARES="1.34.8"
 export PKG_BUILD="curl build-base linux-headers autoconf automake libtool"
 export CFLAGS="-Os"
 export LDFLAGS="-static -s"
@@ -15,13 +15,15 @@ export LDFLAGS="-static -s"
 apk --update upgrade
 apk --update add $PKG_BUILD
 
-#curl -sSL https://github.com/shadowsocks/shadowsocks-libev/releases/download/v$VER_SSLIBEV/shadowsocks-libev-$VER_SSLIBEV.tar.gz | tar xz -C /tmp
+#curl -sSL https://github.com/shadowsocks/shadowsocks-c/releases/download/v$VER_SSLIBEV/shadowsocks-libev-$VER_SSLIBEV.tar.gz | tar xz -C /tmp
 #curl -sSL https://github.com/shadowsocks/simple-obfs/archive/v$VER_SIPOBFS.tar.gz | tar xz -C /tmp
-curl -sSL https://github.com/ARMmbed/mbedtls/archive/mbedtls-$VER_MBEDTLS.tar.gz | tar xz -C /tmp
+#curl -sSL https://github.com/ARMmbed/mbedtls/archive/mbedtls-$VER_MBEDTLS.tar.gz | tar xz -C /tmp
+curl -sSL https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-$VER_MBEDTLS/mbedtls-$VER_MBEDTLS.tar.bz2 | tar -jxvf -C /tmp
 curl -sSL https://download.libsodium.org/libsodium/releases/libsodium-$VER_SODIUM.tar.gz | tar xz -C /tmp
 curl -sSL https://downloads.sourceforge.net/project/pcre/pcre/$VER_PCRE/pcre-$VER_PCRE.tar.gz | tar xz -C /tmp
 curl -sSL http://dist.schmorp.de/libev/Attic/libev-$VER_EV.tar.gz | tar xz -C /tmp
-curl -sSL  https://c-ares.haxx.se/download/c-ares-$VER_CARES.tar.gz | tar xz -C /tmp
+curl -sSL https://github.com/c-ares/c-ares/releases/download/v$VER_CARES/c-ares-$VER_CARES.tar.gz | tar xz -C /tmp
+
 
 cd /tmp/libsodium-$VER_SODIUM/
     ./configure \
